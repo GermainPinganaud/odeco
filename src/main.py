45d54383da7583cb229352e1c8bcfe148705f9df
Germain PINGANAUD <germain.pinganaud@teamwork.net>
2020-12-17 16:03:29 +0000

    facto

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import json
+from os import name
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -11,21 +12,31 @@ with open ("data/output_fields.json") as file:
     output_fields = json.load(file)
 
 
-output = {}
-for site in sites :
+def main ():
+    output = {}
+    for site in sites :
+        if apply_filters(site):
+            site_name = site["nom"]
+            output[site_name] = {}
+            for output_field in output_fields :
+                output[site_name][output_field] = site[output_field]
+
+
+    result = {
+        "filters" : filters,
+        "ouput" : output
+    }
+
+    with open ("data/result.json", "w") as file:
+        json.dump(result, file, indent=4)
+
+
+def apply_filters(site):
     if filters["dep"] in site["cp"][0:2]:
         if filters["type"] == site["site_sous_type"]:
             winds_dir = site["vent_favo"].split(";")
             if filters["wind"] in winds_dir:
-                site_name = site["nom"]
-                output[site_name] = {}
-                for output_field in output_fields :
-                    output[site_name][output_field] = site[output_field]
+                return True
 
-result = {
-    "filters" : filters,
-    "ouput" : output
-}
-
-with open ("data/result.json", "w") as file:
-    json.dump(result, file, indent=4)
+if __name__ == "__main__":
+    main()
