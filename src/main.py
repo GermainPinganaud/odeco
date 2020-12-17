b4cd684a124ac7069cf9002a87c8670f41c76fcf
Germain PINGANAUD <germain.pinganaud@teamwork.net>
2020-12-17 16:19:31 +0000

    facto with dict

--- src/main.py
+++ src/main.py
@@ -1,42 +1,45 @@
 import json
-from os import name
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
-
-with open ("data/sites.json") as file:
-    sites = json.load(file)
-with open ("data/filters.json") as file:
-    filters = json.load(file)
-with open ("data/output_fields.json") as file:
-    output_fields = json.load(file)
 
 
 def main ():
+    sites, filters, output_fields = load_data()
     output = {}
     for site in sites :
-        if apply_filters(site):
-            site_name = site["nom"]
-            output[site_name] = {}
-            for output_field in output_fields :
-                output[site_name][output_field] = site[output_field]
+        if apply_filters(site, filters):
+            output = {
+                site["nom"]: {
+                    output_field : site[output_field] for output_field in output_fields
+                }
+            }
+    write_result(filters, output)
 
 
-    result = {
-        "filters" : filters,
-        "ouput" : output
-    }
+def load_data():
+    with open ("data/sites.json") as file:
+        sites = json.load(file)
+    with open ("data/filters.json") as file:
+        filters = json.load(file)
+    with open ("data/output_fields.json") as file:
+        output_fields = json.load(file)
+    return sites, filters, output_fields
 
-    with open ("data/result.json", "w") as file:
-        json.dump(result, file, indent=4)
 
-
-def apply_filters(site):
+def apply_filters(site, filters):
     if filters["dep"] in site["cp"][0:2]:
         if filters["type"] == site["site_sous_type"]:
             winds_dir = site["vent_favo"].split(";")
             if filters["wind"] in winds_dir:
                 return True
+
+
+def write_result(filters, output):
+    result = {
+        "filters" : filters,
+        "ouput" : output
+    }
+    with open ("data/result.json", "w") as file:
+        json.dump(result, file, indent=4)
+
 
 if __name__ == "__main__":
     main()
